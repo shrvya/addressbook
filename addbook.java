bc3399b745ee32022e7d7415e794b15f9d88421c
shrvya <shrivyashetty@gmail.com>
2021-08-22 21:32:26 +0530

    branch added

--- addbook.java
+++ addbook.java
@@ -26,43 +26,87 @@ public class addbook {
 		System.out.println("email is :"+email);
 		System.out.println("phone number  is :"+phone_number);
 	}
+	
+	public void edit(String fname, String lname, String city, String address, String zip, String email, String phone_number)//method to edit
+	{
+		this.fname=fname;
+		this.lname=lname;
+		this.city=city;
+		this.address=address;
+		this.zip=zip;
+		this.email=email;
+		this.phone_number=phone_number;	
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("welcome to address book"); 
 		 String fname,lname,city,address,zip,email,phone_number;
 		addbook[] ab=new addbook[10];
-        int n;
+       int n=0,c=0,choice;
 		
 		Scanner sc=new Scanner(System.in);
-		
-		System.out.println("how many entries :");//number of entries
-		n=sc.nextInt();
-		for(int i=0;i<n;i++)
+		while(c!=1)
 		{
-			System.out.println("First name :");
-			fname=sc.next();
-			System.out.println("Last name :");
-			lname=sc.next();
-			System.out.println("City name :");
-			city=sc.next();
-			System.out.println("address :");
-			address=sc.next();
-			System.out.println("zip :");
-			zip=sc.next();
-			System.out.println("email :");
-			email=sc.next();
-			System.out.println("phone number :");
-			phone_number=sc.next();
-			ab[i]=new addbook( fname,lname,city,address,zip,email,phone_number);
-		}
-		System.out.println("details::");
-		for(int j=0;j<n;j++)
-		{
-			ab[j].display();	
-		}
+			
+			System.out.println("enter the choice 1.Add 2.edit 3.Display ");
+			choice=sc.nextInt();
+			switch(choice) {
+			case 1:System.out.println("Add the details");//choice=1,then details are entered
+			     System.out.println("First name :");
+				fname=sc.next();
+				System.out.println("Last name :");
+				lname=sc.next();
+				System.out.println("City name :");
+				city=sc.next();
+				System.out.println("address :");
+				address=sc.next();
+				System.out.println("zip :");
+				zip=sc.next();
+				System.out.println("email :");
+				email=sc.next();
+				System.out.println("phone number :");
+				phone_number=sc.next();
+				ab[n]=new addbook( fname,lname,city,address,zip,email,phone_number);
+			    n++;
+			    break;
+			case 2:System.out.println("enter the name of person you want to edit");
+		           String ename=sc.next();
+		      for(int i=0;i<n;i++)//loop to enter n values
+				{
+		    	  if(ab[i].fname.equals(ename))
+		    	  {
+					System.out.println("First name :");
+					fname=sc.next();
+					System.out.println("Last name :");
+					lname=sc.next();
+					System.out.println("City name :");
+					city=sc.next();
+					System.out.println("address :");
+					address=sc.next();
+					System.out.println("zip :");
+					zip=sc.next();
+					System.out.println("email :");
+					email=sc.next();
+					System.out.println("phone number :");
+					phone_number=sc.next();
+					ab[i].edit(fname,lname,city,address ,zip ,email ,phone_number);
+		    	  }
+				}
+		    	  break;
 		
+		    	  case 3:System.out.println("details::");
+		    	         for(int j=0;j<n;j++)
+		  		         {
+		  			       ab[j].display();	
+		  		          }
+		    	          break;
+		    	  default :
+		    		  c=1;
+		    		  break;
+			
 		
+		}
 	}
-
+	}
 }
