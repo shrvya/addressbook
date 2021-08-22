55e30b92a3d357443ddd8f861eac771322dd2a3e
shrvya <shrivyashetty@gmail.com>
2021-08-22 21:48:58 +0530

    branch added

--- addbook.java
+++ addbook.java
@@ -3,7 +3,7 @@ package addbook;
 import java.util.Scanner;
 
 public class addbook {
-	
+	public static int n=0;
 	private String  fname,lname,city,address,zip,email,phone_number;
 	public addbook( String fname, String lname, String city, String address, String zip, String email, String phone_number)//constructor
 	{
@@ -26,6 +26,16 @@ public class addbook {
 		System.out.println("email is :"+email);
 		System.out.println("phone number  is :"+phone_number);
 	}
+	public static void delete(int v,addbook[] ab)//delete the contact 
+	{
+		int i;
+		ab[v]=null;
+		for( i=v;i<n;i++)
+		{
+			ab[i]=ab[i+1];
+		}
+		
+	}
 	
 	public void edit(String fname, String lname, String city, String address, String zip, String email, String phone_number)//method to edit
 	{
@@ -43,7 +53,7 @@ public class addbook {
 		System.out.println("welcome to address book"); 
 		 String fname,lname,city,address,zip,email,phone_number;
 		addbook[] ab=new addbook[10];
-       int n=0,c=0,choice;
+       int c=0,choice;
 		
 		Scanner sc=new Scanner(System.in);
 		while(c!=1)
@@ -72,9 +82,9 @@ public class addbook {
 			    break;
 			case 2:System.out.println("enter the name of person you want to edit");
 		           String ename=sc.next();
-		      for(int i=0;i<n;i++)//loop to enter n values
+		      for(int i=0;i<n;i++)//loop to enter values
 				{
-		    	  if(ab[i].fname.equals(ename))
+		    	  if(ab[i].fname.equals(ename))//checks if name is present 
 		    	  {
 					System.out.println("First name :");
 					fname=sc.next();
@@ -92,6 +102,9 @@ public class addbook {
 					phone_number=sc.next();
 					ab[i].edit(fname,lname,city,address ,zip ,email ,phone_number);
 		    	  }
+		    	  else {
+		    		  System.out.println("Name not found");
+		    	  }
 				}
 		    	  break;
 		
@@ -101,11 +114,21 @@ public class addbook {
 		  			       ab[j].display();	
 		  		          }
 		    	          break;
+		    	  case 4:System.out.println("enter the name to be deleted");
+		    	         String dname=sc.next();
+		    	         for(int i=0;i<n;i++) {
+		    	         if(ab[i].fname.equals(dname))//checks if name is present 
+				    	  {
+		    	        	 int val=i;
+		    	        	 delete(val,ab);
+		    	        	 n--;
+				    	  }
+		    	         }
+		    	         break;
+		    	         
 		    	  default :
 		    		  c=1;
 		    		  break;
-			
-		
 		}
 	}
 	}
