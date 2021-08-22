d1ea08d3ec31cd0180c1ebce26bb7a840fa6f4e5
shrvya <shrivyashetty@gmail.com>
2021-08-22 13:07:43 +0530

    view code

--- addressbook.java
+++ addressbook.java
@@ -1,8 +1,10 @@
 package addressbook;
 
+import java.util.Scanner;
+
 public class addressbook {
-	private String fname,lname,city,address,zip,email,phone_number;
-	public addressbook( String fname, String lname, String city, String address, String zip, String email, String phone_number)
+	private String  fname,lname,city,address,zip,email,phone_number;
+	public void  addressbook( String fname, String lname, String city, String address, String zip, String email, String phone_number)
 	{
 		this.fname=fname;
 		this.lname=lname;
@@ -26,11 +28,40 @@ public class addressbook {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		String fname,lname,city,address ,zip ,email ,phone_number;
 		System.out.println("welcome to address book");//welcome message
-		addressbook ad=new addressbook("Aliya","bhat","maharashtra","mumbai","abc","aliya@gmail.com","987456133");
-		ad.display();
+		int n;
 		
+		Scanner sc=new Scanner(System.in);
+		
+		System.out.println("how many entries :");//number of entries
+		n=sc.nextInt();
+		addressbook ad=new addressbook();
+		for(int i=0;i<n;i++)//loop to enter n values
+		{
+			System.out.println("First name :");
+			ad.fname=sc.next();
+			System.out.println("Last name :");
+			ad.lname=sc.next();
+			System.out.println("City name :");
+			ad.city=sc.next();
+			System.out.println("address :");
+			ad.address=sc.next();
+			System.out.println("zip :");
+			ad.zip=sc.next();
+			System.out.println("email :");
+			ad.email=sc.next();
+			System.out.println("phone number :");
+			ad.phone_number=sc.next();
+			
+			
+		}
+		
+		for(int j=0;j<n;j++)
+		{
+			System.out.println("display contents");
+			ad.display();
+		}
 
 	}
 
