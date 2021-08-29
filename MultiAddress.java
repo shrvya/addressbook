7aedd836c5b374b7a5b7b411d3c1c804edcca0a1
shrvya <shrivyashetty@gmail.com>
2021-08-29 19:00:26 +0530

    added:multiple addressbook can be created

--- MultiAddress.java
+++ MultiAddress.java
@@ -5,61 +5,62 @@ import java.util.Scanner;
 
 public class MultiAddress {
 	static int choice;
-	static Scanner sc=new Scanner(System.in);
-	public static void choose(int choice,addressbook obj)
-	{
-		switch(choice)
-		{
-		case 1:obj.addcontact();
-		      break;
-		case 2:obj.editcontact();
-		      break;
-		case 3:obj.displaycontact();
-		       break;
-		case 4:obj.deletecontact();
-		break;
-		
+	static int counter = 0;
+	static Scanner sc = new Scanner(System.in);
+
+	/*
+	 * this method has 2 parameters this method will use object to call methods from
+	 * addressbook class based on choice parameter
+	 */
+	public static void choose(int choice, addressbook obj) {
+		switch (choice) {
+		case 1:
+			obj.insertContact();
+			break;
+		case 2:
+			obj.editContact();
+			break;
+		case 3:
+			obj.displayContact();
+			break;
+		case 4:
+			obj.deleteContact();
+			break;
+
 		}
 	}
-	
-	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		addressbook obj=new addressbook();
-		Hashtable<String,addressbook> multiaddress= new Hashtable<String,addressbook>();
-		String k="addressbook1";
-		multiaddress.put(k,obj);
-		String addname=" ";
-		int counter=0;
-		while(counter!=1)
-		{
-		System.out.println("enter the choice 1.Add 2.edit 3.Display 4.delete 5.add addressbook 6.exit");
-		choice=sc.nextInt();
-		if(choice==5)
-		{
-		      
-		      System.out.println("Enetr addressbook name");
-		      addressbook obj1=new addressbook();
-		      addname=sc.next();
-		      multiaddress.put(addname,obj1);
+
+		addressbook obj = new addressbook();
+		Hashtable<String, addressbook> multiaddress = new Hashtable<String, addressbook>();
+
+		String k = "addressbook1";
+		multiaddress.put(k, obj);
+
+		String addname = " ";
+
+		while (counter != 1) {
+			System.out.println("enter the choice 1.Add 2.edit 3.Display 4.delete 5.add addressbook 6.exit");
+			choice = sc.nextInt();
+			if (choice == 5) {
+
+				System.out.println("Enetr addressbook name");
+				addressbook obj1 = new addressbook();
+				addname = sc.next();
+				multiaddress.put(addname, obj1);
+			} else if (choice == 6) {
+				counter = 1;
+				System.out.println("exited");
+
+			} else {
+				System.out.println("Enter addressbook name1::");
+				addname = sc.next();
+				choose(choice, multiaddress.get(addname));
+			}
+
 		}
-		else if(choice==6)
-		{
-			counter=1;
-			System.out.println("exited");
-		      
-		      
-		}
-		else {
-			System.out.println("Enter addressbook name1::");
-			addname=sc.next();
-			choose(choice, multiaddress.get(addname));
-		}
-		
-		}
-		
 
 	}
 
--- addressbook.java
+++ addressbook.java
@@ -1,115 +1,134 @@
 package addressbook;
+
+import java.util.ArrayList;
 import java.util.Scanner;
+
+/*
+ * this class will allow to add ,edit,delete,display contacts in a address book
+ */
 public class addressbook {
-	public static int n=0;
-	private String  fname,lname,city,address,zip,email,phone_number;
-	public addressbook( String fname, String lname, String city, String address, String zip, String email, String phone_number)
-	{
-		this.fname=fname;
-		this.lname=lname;
-		this.city=city;
-		this.address=address;
-		this.zip=zip;
-		this.email=email;
-		this.phone_number=phone_number;	
+	public static int n = 0;
+	Scanner sc = new Scanner(System.in);
+
+	public ArrayList<addressbook> contact = new ArrayList<>();
+	public String fname, lname, city, address, zip, email, phone_number;
+
+	/*
+	 * this is a constructor to initialize parameters
+	 */
+	public addressbook(String fname, String lname, String city, String address, String zip, String email,
+			String phone_number) {
+		this.fname = fname;
+		this.lname = lname;
+		this.city = city;
+		this.address = address;
+		this.zip = zip;
+		this.email = email;
+		this.phone_number = phone_number;
 	}
-	public void  edit( String fname, String lname, String city, String address, String zip, String email, String phone_number)
-	{
-		this.fname=fname;
-		this.lname=lname;
-		this.city=city;
-		this.address=address;
-		this.zip=zip;
-		this.email=email;
-		this.phone_number=phone_number;	
+
+	public addressbook() {
+		// TODO Auto-generated constructor stub
 	}
-	public void display()
-	{
-		System.out.println("first name is :"+fname);
-		System.out.println("last name is :"+lname);
-		System.out.println("city name is :"+city);
-		System.out.println("address is :"+address);
-		System.out.println("zip is :"+zip);
-		System.out.println("email is :"+email);
-		System.out.println("phone number  is :"+phone_number);
+
+	/*
+	 * this method is used to insert information into address book info like first
+	 * ,last name,email,phone number etc can be inserted into address book
+	 */
+	public void insertContact() {
+		System.out.println("Add the details");
+
+		System.out.println("First name :");
+		fname = sc.next();
+		System.out.println("Last name :");
+		lname = sc.next();
+		System.out.println("City name :");
+		city = sc.next();
+		System.out.println("address :");
+		address = sc.next();
+		System.out.println("zip :");
+		zip = sc.next();
+		System.out.println("email :");
+		email = sc.next();
+		System.out.println("phone number :");
+		phone_number = sc.next();
+		contact.add(new addressbook(fname, lname, city, address, zip, email, phone_number));
+
 	}
-	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		String fname,lname,city,address ,zip ,email ,phone_number;
-		System.out.println("welcome to address book");//welcome message
-		
-		
-		Scanner sc=new Scanner(System.in);
-		
-		
-		
-		addressbook[] ad1=new addressbook[10];
-		int choice,c=0;
-		while(c!=1) {
-		System.out.println("enter the choice 1.Add 2.edit 3.Display");
-		choice=sc.nextInt();
-		switch(choice)
+
+	/*
+	 * this method will allow user to edit already present contact details it will
+	 * ask user to enter the name of the person who's contact details is to be
+	 * edited if the name is present then it will allow user to edit details
+	 * otherwise it will display that name is absent in addressbook
+	 */
+	public void editContact() {
+		System.out.println("enter the name of person you want to edit");
+		String name = sc.next();
+		for (int i = 0; i < contact.size(); i++)// loop to enter n values
 		{
-		case 1:System.out.println("Add the details");
-		
-			System.out.println("First name :");
-			fname=sc.next();
-			System.out.println("Last name :");
-			lname=sc.next();
-			System.out.println("City name :");
-			city=sc.next();
-			System.out.println("address :");
-			address=sc.next();
-			System.out.println("zip :");
-			zip=sc.next();
-			System.out.println("email :");
-			email=sc.next();
-			System.out.println("phone number :");
-			phone_number=sc.next();
-			ad1[n]=new addressbook(fname,lname,city,address,zip,email,phone_number);
-		    n++;
-		    break;
-		case 2:System.out.println("enter the name of person you want to edit");
-		      String name=sc.next();
-		      for(int i=0;i<n;i++)//loop to enter n values
-				{
-		    	  if(ad1[i].fname.equals(name)) {
-					System.out.println("First name :");
-					fname=sc.next();
-					System.out.println("Last name :");
-					lname=sc.next();
-					System.out.println("City name :");
-					city=sc.next();
-					System.out.println("address :");
-					address=sc.next();
-					System.out.println("zip :");
-					zip=sc.next();
-					System.out.println("email :");
-					email=sc.next();
-					System.out.println("phone number :");
-					phone_number=sc.next();
-					ad1[i].edit(fname,lname,city,address ,zip ,email ,phone_number);
-		    	  }
-		    	  else {
-		    		  System.out.println("name not found");
-		    	  }	
-				}
-		      break;
-		case 3: if(n==0)
-		{
-			System.out.println("no entries");
+			addressbook obj = contact.get(i);
+			if (obj.fname.equals(name)) {
+				System.out.println("First name :");
+				obj.fname = sc.next();
+				System.out.println("Last name :");
+				obj.lname = sc.next();
+				System.out.println("City name :");
+				obj.city = sc.next();
+				System.out.println("address :");
+				obj.address = sc.next();
+				System.out.println("zip :");
+				obj.zip = sc.next();
+				System.out.println("email :");
+				obj.email = sc.next();
+				System.out.println("phone number :");
+				obj.phone_number = sc.next();
+
+			} else {
+				System.out.println("name not found");
+			}
 		}
-			for(int j=0;j<n;j++)
-				{
-					System.out.println("display contents");
-					ad1[j].display();
-				}
-		          break;
-		default:c=1;
-		        		  break;
+	}
+
+	/*
+	 * this method will display the contact details in addressbook
+	 */
+	public void displayContact() {
+		if (contact.size() == 0) {
+			System.out.println("empty adddressbook!!!");
+		} else {
+			System.out.println("Contents of addressbook:");
+			for (int j = 0; j < contact.size(); j++) {
+				addressbook obj = contact.get(j);
+				System.out.println("first name is :" + obj.fname);
+				System.out.println("last name is :" + obj.lname);
+				System.out.println("city name is :" + obj.city);
+				System.out.println("address is :" + obj.address);
+				System.out.println("zip is :" + obj.zip);
+				System.out.println("email is :" + obj.email);
+				System.out.println("phone number  is :" + obj.phone_number);
+			}
+
 		}
 
+	}
+
+	/*
+	 * this method will delete the specified contact from addressbook users enters
+	 * the name of person who's contact is to be deleted array list has method
+	 * called remove to remove entries
+	 */
+	public void deleteContact() {
+		System.out.println("Enter the person whose contact to be deleted");
+		String name11 = sc.next();
+
+		for (int j = 0; j < contact.size(); j++) {
+			addressbook object = contact.get(j);
+			if (object.fname.equals(name11)) {
+
+				contact.remove(object);
+
+			}
 		}
 
 	}
